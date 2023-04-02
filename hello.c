3774955139a2b2a542108e8fcb79418981303b08
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2023-04-02 17:07:41 +0600

    added a new line

--- hello.c
+++ hello.c
@@ -5,6 +5,7 @@ printf("hello world \n");
 printf("hello codder\n");
 printf("start your first code\n");
 printf("this is a printf fuction use for show somthing");
+printf("I am starting code");
 
 
     return 0;
